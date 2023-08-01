0d37b5b2c2b4c197f0e72e1a905473e094db039c
Joe Dumford <dumford.joe@gmail.com>
2023-08-01 08:57:31 -0400

    Styling and trucating the podcast description

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -11,26 +11,24 @@ const Footer = () => {
                 <div className="Footer-holder">
                     <div className="Footer-icons">
                         <a href="https://www.instagram.com/thenamelessapes/" target="_blank" rel="noreferrer">
-              <span className="Footer-icons">
-
-                <i className="icon-instagram-1" />
-
-              </span>
+                            <span className="Footer-icons">
+                                <i className="icon-instagram-1" />
+                            </span>
                         </a>
                         <a href="https://www.facebook.com/profile.php?id=100094589184399/" target="_blank" rel="noreferrer">
-              <span className="Footer-icons">
-                <i className="icon-facebook" />
-              </span>
+                            <span className="Footer-icons">
+                                <i className="icon-facebook" />
+                            </span>
                         </a>
                         <a href="https://open.spotify.com/artist/6Q6DDbUO3LpVD6WhRyzg8M" target="_blank" rel="noreferrer">
-              <span className="Footer-icons">
-                <i className="icon-spotify" />
-              </span>
+                            <span className="Footer-icons">
+                                <i className="icon-spotify" />
+                            </span>
                         </a>
                         <a href="https://soundcloud.com/joe-dumford" target="_blank" rel="noreferrer">
-              <span className="Footer-icons">
-                <i className="icon-soundcloud" />
-              </span>
+                            <span className="Footer-icons">
+                                <i className="icon-soundcloud" />
+                            </span>
                         </a>
                     </div>
                     <p className="footer-copywrite">© 2019 Nameless Apes. All Rights Reserved</p>
--- src/components/UI/Card/Card.jsx
+++ src/components/UI/Card/Card.jsx
@@ -1,39 +1,3 @@
-// import React, { useState } from 'react';
-// import PropTypes from 'prop-types';
-//
-// const Card = ({ image, title, price, description }) => {
-//
-//     const [isDescriptionExpanded, setIsDescriptionExpanded] = useState(false);
-//
-//     const toggleDescription = () => {
-//         setIsDescriptionExpanded((prevExpanded) => !prevExpanded);
-//     };
-//
-//     return (
-//         <div className="card">
-//             <img src={image} alt={description} />
-//             <div className="card-details">
-//                 <h2>{title}</h2>
-//                 <p style={{ maxHeight: isDescriptionExpanded ? 'none' : '70px' }}>{description}</p>
-//                 <span className="expand-icon" onClick={toggleDescription}>
-//                     {isDescriptionExpanded ? '▲' : '▼'}
-//                 </span>
-//                 <p>Price: ${price}</p>
-//             </div>
-//         </div>
-//     );
-// };
-//
-// Card.propTypes = {
-//     image: PropTypes.string.isRequired,
-//     title: PropTypes.string.isRequired,
-//     price: PropTypes.number,
-//     description: PropTypes.string.isRequired,
-// };
-//
-// export default Card;
-
-
 import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
@@ -49,15 +13,25 @@ const Card = ({ image, title, price, description }) => {
             <img src={image} alt={description} />
             <div className="card-details">
                 <h2>{title}</h2>
-                <div
-                    className={`description-container ${isDescriptionExpanded ? 'expanded' : ''}`}
-                >
-                    <span className="expand-icon" onClick={toggleDescription}>
-                        {isDescriptionExpanded ? '▲' : '▼'}
-                    </span>
-                        <p>{description}</p>
+                <div className={`description-container ${isDescriptionExpanded ? 'expanded' : ''}`}>
+                    <p
+                        className={`description-text ${isDescriptionExpanded ? 'expanded' : ''}`}
+                        style={{ whiteSpace: isDescriptionExpanded ? 'normal' : 'nowrap' }}
+                    >
+                        {description}
+                    </p>
+                    {!isDescriptionExpanded && (
+                        <span className="read-more" onClick={toggleDescription}>
+                            Read more
+                        </span>
+                    )}
                 </div>
-                <p>Price: ${price}</p>
+                {isDescriptionExpanded && (
+                    <span className="expand-icon" onClick={toggleDescription}>
+                        ▲
+                    </span>
+                )}
+                {!isDescriptionExpanded && <p>Price: ${price}</p>}
             </div>
         </div>
     );
